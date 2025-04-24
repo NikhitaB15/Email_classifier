fba99ba551e98ae4a1822df404321e3450f19456
Nikhita Bhattacharya <107978350+NikhitaB15@users.noreply.github.com>
2025-04-24 11:07:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 pip install --upgrade pip
-pip install -r requirements.txt --no-cache-dir
+pip install -r requirements.txt --no-cache-dir --ignore-installed
 python -m spacy download en_core_web_sm
+
